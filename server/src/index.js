8f010c603bf8704d4b9e7d53f811a38e0c077b6d
hardik jain <hardikj2003@gmail.com>
2026-04-15 21:58:52 +0530

    cors configered

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,16 @@ import axios from "axios"
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://curalink-frontend-j201.onrender.com", 
+    "http://localhost:5173",                    
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"],
+  credentials: true
+}));
+app.options('*', cors());
 app.use(express.json());
 
 // Database
